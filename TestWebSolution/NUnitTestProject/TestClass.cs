31eee240c8f23524ca5d634d45879f8dd3972806
Acharya <rakesh.achar@accionlabs.com>
2020-09-23 23:20:24 +0530

    updated test case

---
+++ TestWebSolution/NUnitTestProject/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void NunitTestMethod1()
+        {
+            Assert.AreEqual(20, 20);
+        }
+
+        [Test]
+        public void NunitTestMethod2()
+        {
+            Assert.AreEqual(40, 20);
+        }
+
+    }
+}
--- TestWebSolution/UnitTestProject1/UnitTest1.cs
+++ TestWebSolution/UnitTestProject1/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual(90, 80);
+            Assert.AreEqual(80, 80);
         }
     }
 }
