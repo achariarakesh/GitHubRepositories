1768f9f9bf456f6dd0693ad8191bf77043520adb
Acharya <rakesh.achar@accionlabs.com>
2020-09-23 10:40:07 +0530

    added test cases

---
+++ TestWebSolution/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(20, 20);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(20, 40);
+        }
+    }
+}
