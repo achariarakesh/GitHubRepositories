cc27ad1475514e2ba71c0e6344bc8b9c4ce57102
Acharya <rakesh.achar@accionlabs.com>
2020-09-23 21:02:29 +0530

    updated testcase

--- TestWebSolution/UnitTestProject1/UnitTest1.cs
+++ TestWebSolution/UnitTestProject1/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual(40, 40);
+            Assert.AreEqual(80, 80);
         }
     }
 }
